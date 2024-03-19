83bd186a8a22cad6c8cfc24dc2838f5eade01cc8
APPLE <apple@MacBook-Pro.local>
2024-03-19 10:23:26 +0800

    增加字段传入,过滤字段传入

--- fofa.py
+++ fofa.py
@@ -11,21 +11,35 @@ from utils.time_tools import *
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 class FofaSearch:
+    """
+    如果一天更新资产超过1w那超过的部分就没辙了
+    """
 
-    def __init__(self, query_str):
+    def __init__(self, query_str, fields='ip,host,port,link,domain', deduplicate_field='host'):
+        """
+        :param query_str: 查询字符串
+        :param fields: 查询字段
+        :param deduplicate_field: 去重字段, 需要在fields中出现
+        """
         self.key = FOFA_KEY
         self.email = FOFA_EMAIL
         self.query_str = query_str
-        self.query = '(' + query_str + ') && country="CN" && status_code="200"'
+        self.query = query_str  # self.query = '(' + query_str + ') && country="CN" && status_code="200"'
+        self.fields = fields  # ip,port,host,link,domain -> https://fofa.info/api/batches_pages
+        if deduplicate_field not in fields:
+            raise Exception('需要传入去重的字段, 并且该字段在fields中')
+        self.deduplicate_field = deduplicate_field
 
-    def get_all_links(self):
+    def get_all_assets(self):
         print(self.query)
-        return self.get_all_links_recursion(time_format_to_timestamp(increase(now_timestamp(), 1)))
+        return self.get_all_assets_recursion(time_format_to_timestamp(increase(now_timestamp(), 1)))
 
-    def get_all_links_recursion(self, timestamp_before):
-        total = self.get_total_size()
-        print('fofa查询获取总数量: ' + str(total))
-        links = set()
+    def get_all_assets_recursion(self, timestamp_before):
+        total_size = self.get_total_size()
+        size_counter = 0  # 累加获取的数量
+        print('fofa查询获取总数量: ' + str(total_size))
+        deduplicate_set = set()  # 用于去重的集合
+        assets = []  # 最终返回的资产
         o_right = 365  # 初始二分查找跨度可稍微设置大一点, 程序自动调整该大小
         o_left = 0
         target = 10000
@@ -33,13 +47,13 @@ class FofaSearch:
             query = '{query} && before="{before}"'.format(query=self.query, before=timestamp_to_time_format(timestamp_before))
             result = self.my_fofa(query)
             if result is not None:
-                for link in result['results']:
-                    links.add(link)
-                last_size = int(result['size'])
+                size = self.add_assets(result, deduplicate_set, assets)
+                last_total_size = size['last_total_size']
+                size_counter += size['this_size']
 
-                if last_size == 0:
+                if last_total_size == 0:
                     break
-                print('fofa查询获取link的数量 before->{before}: {num}'.format(before=timestamp_to_time_format(timestamp_before), num=str(len(result['results']))))
+                print('fofa查询本次获取数量 before->{before}: {num}'.format(before=timestamp_to_time_format(timestamp_before), num=str(len(result['results']))))
 
                 # right值动态调整, 找到下一个1w条的最小before
                 # 三种情况
@@ -55,8 +69,8 @@ class FofaSearch:
                     query = '{query} && before="{before}"'.format(query=self.query, before=decrease(timestamp_before, mid))
                     result = self.my_fofa(query, size=1)
                     if result is not None:
-                        next_size = int(result['size'])
-                        diff = last_size - next_size
+                        next_total_size = int(result['size'])
+                        diff = last_total_size - next_total_size
                         # print(diff)
                         if diff == target:
                             right = mid
@@ -71,15 +85,16 @@ class FofaSearch:
                 o_right = diff_timestamp(last_timestamp_before, timestamp_before) * 2
 
                 # 数量过少的情况
-                if len(links) >= total:
+                if size_counter >= total_size:
                     break
-        print('去重后已获取: ' + str(len(links)))
-        return links
+        print('去重后已获取: ' + str(len(assets)))
+        return assets
 
-    def my_fofa(self, query=None, size=10000):
+    def my_fofa(self, query=None, size=10000, fields=None):
         query = self.query if query is None else query
+        fields = self.fields if fields is None else fields
         qbase64 = base64.b64encode(query.encode('utf-8'))
-        url = 'https://fofa.info/api/v1/search/all?email={email}&key={key}&size={size}&fields=link&qbase64={qbase64}&full=true'.format(email=self.email, key=self.key, size=size, qbase64=qbase64.decode('utf'))
+        url = 'https://fofa.info/api/v1/search/all?email={email}&key={key}&size={size}&fields={fields}&qbase64={qbase64}&full=true'.format(email=self.email, key=self.key, fields=fields, size=size, qbase64=qbase64.decode('utf'))
         data = Request().get(url).data
         return self.handle_fofa_result(data)
 
@@ -107,3 +122,26 @@ class FofaSearch:
         """
         size = self.my_fofa(size=1)['size']
         return int(size)
+
+    def add_assets(self, result, deduplicate_set, assets):
+        """
+        去重, 添加资产, 返回result中的数量
+        :param result: 单次查询结果
+        :param deduplicate_set: 用于去重的数据
+        :param assets: 总资产
+        :return: (本次获取未去重数量, 总共的资产数)
+        """
+        idx = self.fields.split(',').index(self.deduplicate_field)
+        for asset in result['results']:
+            deduplicate_data = ''
+            if isinstance(asset, list):
+                deduplicate_data = asset[idx]  # 多字段
+            else:
+                deduplicate_data = asset  # 单字段
+
+            if deduplicate_data not in deduplicate_set:
+                assets.append(asset)
+                deduplicate_set.add(deduplicate_data)
+
+        # (本次获取未去重数量, 总共的资产数)
+        return {'this_size': len(result['results']), 'last_total_size': result['size']}
--- test.py
+++ test.py
@@ -1,13 +1,41 @@
+import re
+
 from fofa import FofaSearch
 
+
+def jd():
+    # 奇怪了2023-04-05和2023-04-04一天差了14w....这个真没办法了
+    hosts = FofaSearch('domain="jd.com"', fields='link', deduplicate_field='link').get_all_assets()
+    with open('domains.txt', 'w') as f:
+        for host in hosts:
+            r = re.compile('^(https?://)?([.\w-]*)(:\d{,8})?').match(host)
+            if r:
+                f.write(r.group(2) + '\n')
+
+
+def re_test():
+    # 提取域名和ip
+    s = ['http://a.com:80',
+         'http://b.com',
+         'http://1.1.1.1:80',
+         'http://1.1.1.2',
+         '1.1.1.3',
+         '1.1.1.4:65535',
+         'c.com:80',
+         'd.com']
+    p = re.compile('^(https?://)?([.\w-]*)(:\d{,8})?')
+    for u in s:
+        r = p.match(u)
+        print(r.group(2))
+
+
 if __name__ == '__main__':
-    # FofaSearch('app = "wspx"').get_all_links()
-    FofaSearch('app="用友-U8-Cloud"').get_all_links()
-    # FofaSearch('app="log4j2"').get_all_links()
+    jd()
+    # FofaSearch(query_str='app = "wspx"', deduplicate_field='domain').get_all_assets()
+    # FofaSearch('domain="jd.com"', fields='domain,title,link', deduplicate_field='domain').get_all_assets()
+    # FofaSearch('app="log4j2"').get_all_assets()
 
     # now = now_timestamp()
     # before = time_format_to_timestamp('2018-01-01')
     # days = diff_timestamp(now, before)
     # print(days)
-
-
